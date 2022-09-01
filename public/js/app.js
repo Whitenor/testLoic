5075301d514218c6d9c2f7930fb56473f1d0e622
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-01 18:29:03 +0200

    final update

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,17 @@
 let linkCss = document.querySelector('head > link')
-function createElement(typeElement, elementID, elementIDLocation, elementClass, elementSrc, elementTextContent){
+let app = document.querySelector('#app');
+function createElement(typeElement, elementID, elementIDLocation, elementClass, elementSrc, elementTextContent, elementAlt){
     let createElement = document.createElement(typeElement);
     createElement.id = elementID;
     createElement.classList = elementClass;
     createElement.src = elementSrc;
+    createElement.alt = elementAlt;
     createElement.textContent = elementTextContent;
     document.getElementById(elementIDLocation).append(createElement);
+}
+function randomPricing(){
+    randomPrice = (Math.random()*100)+1;
+    return randomPrice.toFixed(2);
 }
 // add css bootstrap
 linkCss.insertAdjacentHTML('beforebegin', '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">')
@@ -15,7 +21,15 @@ fetch('public/php/data.php').then(
     response => response.text().then(
         data =>{
             data.replaceAll('\\', '')
-            test = JSON.parse(data);
-
-        }
-    ))
+            dataParse = JSON.parse(data);
+            for (let i = 0; i < 20; i++) {
+                let random = randomPricing();
+                createElement("div", "product"+i, "app", "card", "","","");
+                createElement("img", "", "product"+i , "card-img-top", "public/images/cardImage.svg", "", "image produit");
+                createElement("div", "descProduct"+i, "product"+i, "card-body d-flex flex-column justify-content-around align-items-center", "","","");
+                createElement("h5", "", "descProduct"+i,"card-title", "","Product"+i,"")
+                createElement("p", "", "descProduct"+i,"text-center", "",dataParse.desc,"");
+                createElement("p", "", "descProduct"+i,"", "","Prix:","");
+                createElement("p", "", "descProduct"+i,"", "",random+" €" ,"");
+            }
+        }))
