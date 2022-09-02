03f7856db64148ad3c58016568fc43c62d910d3c
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-02 11:09:41 +0200

    update for more fitting to instruction

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 let linkCss = document.querySelector('head > link')
 let app = document.querySelector('#app');
+// function for creating an element with many possibilities
 function createElement(typeElement, elementID, elementIDLocation, elementClass, elementSrc, elementTextContent, elementAlt){
     let createElement = document.createElement(typeElement);
     createElement.id = elementID;
@@ -9,28 +10,29 @@ function createElement(typeElement, elementID, elementIDLocation, elementClass, 
     createElement.textContent = elementTextContent;
     document.getElementById(elementIDLocation).append(createElement);
 }
-function randomPricing(){
-    randomPrice = (Math.random()*100)+1;
-    return randomPrice.toFixed(2);
-}
 // add css bootstrap
 linkCss.insertAdjacentHTML('beforebegin', '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">')
 // add js bootstrap
 document.querySelector('body >script').insertAdjacentHTML('beforebegin', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>')
+// retrieving data from data.php
 fetch('public/php/data.php').then(
     response => response.text().then(
         data =>{
+            // deleting useless \
             data.replaceAll('\\', '')
+            // parsing to a json format
             dataParse = JSON.parse(data);
-            for (let i = 0; i < 20; i++) {
-                let random = randomPricing();
-                createElement("div", "product"+i, "app", "card", "","","");
-                createElement("img", "", "product"+i , "card-img-top", "public/images/cardImage.svg", "", "image produit");
-                createElement("div", "descProduct"+i, "product"+i, "card-body d-flex flex-column justify-content-around", "","","");
-                createElement("h5", "", "descProduct"+i,"card-title", "","Product "+i,"")
-                createElement("p", "", "descProduct"+i,"", "",dataParse.desc,"");
+            // loop for creation Of all Card with the previously retrieved data 
+            for (let i = 0; i < dataParse.length; i++) {
+                createElement("div", dataParse[i].ID, "app", "card", "","","");
+                createElement("img", "", dataParse[i].ID , "card-img-top", "public/images/cardImage.svg", "", "image produit");
+                createElement("div", "descProduct"+i, dataParse[i].ID, "card-body d-flex flex-column justify-content-around", "","","");
+                createElement("h5", "", "descProduct"+i,"card-title", "",dataParse[i].title,"")
+                createElement("p", "", "descProduct"+i,"", "",dataParse[i].desc,"");
                 createElement("p", "", "descProduct"+i,"", "","Prix:","");
-                createElement("p", "", "descProduct"+i,"", "",random+" €" ,"");
+                createElement("p", "", "descProduct"+i,"", "",dataParse[i].pricing+" €" ,"");
+                createElement("p", "", "descProduct"+i,"", "","ID:","");
+                createElement("p", "", "descProduct"+i,"", "",dataParse[i].ID,"");
                 createElement("button", "", "descProduct"+i,"btn btn-primary", "","En Savoir Plus","")
             }
-        }))
+}))
--- public/php/data.php
+++ public/php/data.php
@@ -1,3 +1,12 @@
 <?php 
-$test =  array("desc" => "Amet et sed dolores justo sed eirmod quod amet dolor");
+$desc = "Amet et sed dolores justo sed eirmod quod amet dolor";
+$test =  array();
+
+for ($i=0; $i < 50; $i++) { 
+    $adding = array("title" => "Product ".($i+1),
+                    "desc" => $desc,
+                    "pricing" => strval(rand(100,10000)/100),
+                    "ID" => strval(rand(1000, 10000)));
+    array_push($test, $adding);
+}
 echo json_encode($test);
